b871f92d9e470a7beb0fc0815cfba1fd28870cdc
Jude Bradbury <jude@earthguardians.life>
2024-12-01 19:41:16 +1100

    day one complete

--- Day 1/main.go
+++ Day 1/main.go
@@ -6,8 +6,12 @@ import (
 	"math"
 )
 
-func main() {
-	fmt.Println("start")
+/*
+*
+This solves the first part of the issue.
+*/
+func partOne() {
+	fmt.Println("Part one answer:")
 
 	// read in the input data
 	input := helpers.ReadInput()
@@ -27,7 +31,54 @@ func main() {
 		totalDistance += int(math.Abs(float64(element - setTwoPair)))
 
 	}
-
+	fmt.Println("Total distance:")
 	fmt.Println(totalDistance)
+}
 
+/*
+*
+
+	This solves the second part of the issue (didn't spend more than a few seconds thinking up the complexity so don't judge)
+*/
+func partTwo() {
+	fmt.Println("Part two answer:")
+
+	// read in the input data.
+	input := helpers.ReadInput()
+
+	visitedNumbers := make(map[int]int)
+
+	similarity := 0
+
+	//iterate the first array.
+	for _, element1 := range input["setOne"] {
+
+		numOccurrences := visitedNumbers[element1]
+
+		// check if number in map.
+		if numOccurrences == 0 {
+			// doesn't exist yet, find it.
+
+			// count number of occurrences.
+			for _, element2 := range input["setTwo"] {
+				if element1 == element2 {
+					numOccurrences++
+				}
+			}
+			// add to map in case it occurs again.
+			visitedNumbers[element1] = numOccurrences
+		}
+		// if already exists, increment again for repeated digit.
+		// if doesn't already exist, increment for counted occurrences.
+		similarity += numOccurrences * element1
+	}
+
+	fmt.Println("Similarity Score:")
+	fmt.Println(similarity)
+
+}
+
+func main() {
+	partOne()
+	partTwo()
 }
